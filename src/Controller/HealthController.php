471ef86aa3ecb5126c8f6c6444a55d258a672f52
DyaZix-Code <blancosacha38@gmail.com>
2023-11-30 14:17:05 +0100

    health controller

---
+++ src/Controller/HealthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HealthController extends AbstractController
+{
+    #[Route('/health', name: 'app_health')]
+    public function index(): Response
+    {
+        return $this->json('App is running !');
+    }
+}
