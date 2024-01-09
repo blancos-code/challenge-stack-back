a45aa21a071413fca2dfb724549836de99c6f156
JeejaFR <jeremy.defuentes@gmail.com>
2024-01-09 14:53:43 +0100

    fix: edition producteur mieux

--- src/Controller/Admin/ProducteurCrudController.php
+++ src/Controller/Admin/ProducteurCrudController.php
@@ -7,6 +7,7 @@ use App\Entity\Producteur;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -32,16 +33,16 @@ class ProducteurCrudController extends AbstractCrudController
     {
         return [
             AssociationField::new('utilisateur', 'Utilisateur'),
-            CollectionField::new('produits', 'Produit')
-            ->setFormTypeOptions([
-                'by_reference' => false, // Nécessaire pour manipuler la collection
-                'delete_empty' => true, // Supprimer les éléments vides (non utilisés)
-                'allow_delete' => false, // Désactiver la suppression d'éléments
-                'allow_add' => false,
-                'entry_options' => [
-                    'disabled' => true, // Désactiver l'édition des éléments
-                ],
-            ]),
+            ArrayField::new('produits', 'Produit')
+                ->setFormTypeOptions([
+                    'by_reference' => false,
+                    'delete_empty' => true,
+                    'allow_delete' => false,
+                    'allow_add' => false, // Désactiver l'ajout d'éléments
+                    'entry_options' => [
+                        'disabled' => true,
+                    ],
+                ]),
             TextEditorField::new('description'),
         ];
     }
