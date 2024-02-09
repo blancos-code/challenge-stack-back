b9c4e06959ee813b418653be2931edc0241de6f1
JeejaFR <jeremy.defuentes@gmail.com>
2024-02-09 12:28:34 +0100

    fix: apiPlatform envoie prixProduits

--- src/Entity/PrixProduits.php
+++ src/Entity/PrixProduits.php
@@ -2,25 +2,32 @@
 
 namespace App\Entity;
 
-use App\Repository\PrixProduitsRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\PrixProduitsRepository;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PrixProduitsRepository::class)]
+#[ApiResource(normalizationContext: ['groups' => ['read']], denormalizationContext: ['groups' => ['write']])]
 class PrixProduits
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(["read", "write"])]
     private ?int $id = null;
 
     #[ORM\ManyToOne(inversedBy: 'prixProduits')]
+    #[Groups(["read", "write"])]
     private ?Produit $produit = null;
 
     #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    #[Groups(["read", "write"])]
     private ?string $prix = null;
 
     #[ORM\ManyToOne(inversedBy: 'prixProduit')]
+    #[Groups(["read", "write"])]
     private ?Producteur $producteur = null;
 
     public function getId(): ?int
